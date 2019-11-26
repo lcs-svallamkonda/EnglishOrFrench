6d0faf15453b4de4c72debbb71e0f20cd38b4640
Sunaina Vallamkonda <svallamkonda@lcs.on.ca>
2019-11-26 10:44:14 -0500

    Initial Commit

---
+++ EnglishOrFrench/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EnglishOrFrench
+//
+//  Created by Vallamkonda, Sunaina on 2019-11-26.
+//  Copyright © 2019 Vallamkonda, Sunaina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
